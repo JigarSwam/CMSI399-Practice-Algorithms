a4023c32c37d28e4630240648848fa8e0558030a
JigarSwam <jswaminarayan@gmail.com>
2020-04-02 19:36:41 -0700

    Edit formatting

--- charactersByFrequency.js
+++ charactersByFrequency.js
@@ -8,12 +8,12 @@ var frequencySort = function(s) {
     let result = '';
     let stringArray = s.split('');
 
-    for(let i=0;i<stringArray.length;i++){
+    for(let i = 0; i < stringArray.length; i++){
     	letterMap[stringArray[i]] = letterMap[stringArray[i]] + 1 || 1;
     }
 
     Object.keys(letterMap).sort((a,b)=>letterMap[b]-letterMap[a]).forEach((v) => {
-    	for(let j=0;j<letterMap[v];j++){
+    	for(let j = 0; j < letterMap[v]; j++){
     		result += v;
     	}
     });
