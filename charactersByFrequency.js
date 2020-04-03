ef1d460800952a7184b85bfc17a6ba7a5ff8bb56
JigarSwam <jswaminarayan@gmail.com>
2020-04-02 18:34:47 -0700

    Complete characterByFrequency.

---
+++ charactersByFrequency.js
@@ -0,0 +1,22 @@
+/**
+ * Practice Algorithm for Thursday, April 2nd.
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    let letterMap = {};
+    let result = '';
+    let stringArray = s.split('');
+
+    for(let i=0;i<stringArray.length;i++){
+    	letterMap[stringArray[i]] = letterMap[stringArray[i]] + 1 || 1;
+    }
+
+    Object.keys(letterMap).sort((a,b)=>letterMap[b]-letterMap[a]).forEach((v) => {
+    	for(let j=0;j<letterMap[v];j++){
+    		result += v;
+    	}
+    });
+    
+    return result;
+};
